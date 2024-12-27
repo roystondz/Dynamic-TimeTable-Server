8a4dfd161914a0d2cc6e7c31238f9cf74429ccce
Royston <roystonad2004@gmail.com>
2024-12-27 21:26:21 +0530

    register form

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,3 +3,27 @@ const studentModel = require('../models/student-model');
 const jwt = require("jsonwebtoken");
 const cookieParser =  require("cookie-parser");
 const bcrypt = require('bcrypt');
+
+
+ module.exports.registerStudent = async function(req,res){
+    try{
+        let {name, usn, semester, section, branch} = req.body;
+    let student =  await studentModel.findOne({usn});
+
+    if(student){
+        return res.status(400).json({message: "Student already exists"});
+    }
+    if(!student){
+        let newStudent = await studentModel.create({
+            name,
+            usn,
+            semester,
+            section,
+            branch
+        })
+        
+    }
+    }catch(err){
+        console.log(err);
+    }
+ }
