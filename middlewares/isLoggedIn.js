87f9a1ded37dc458900851125b3d457914e400b5
Royston <roystonad2004@gmail.com>
2024-12-28 18:50:53 +0530

    JWT token

---
+++ middlewares/isLoggedIn.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+const studentModel = require("../models/student-model");
+
+module.exports = async function (req,res,next){
+    if(!req.cookies){
+        req.flash("error","User must be logged in");
+        res.redirect("/");
+    }
+    try{
+        let decodedKey = jwt.verify(req,cookies.token,process.env.JWT_KEY);
+        let searchStudent = await studentModel.findOne({usn:decodedKey.usn});
+        req.student=student;
+        next();
+    }catch(err){
+        req.flash("sww","Something went wrong");
+        res.redirect("/");
+    }
+}
